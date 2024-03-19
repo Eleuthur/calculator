15ca822c3c32db615676df8a3a8d41cf33192cdb
Eleuthur <105822177+Eleuthur@users.noreply.github.com>
2024-03-19 14:41:54 +0000

    created calcuator display - added function to choose correct math function

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ function multiply(a, b) {
 function divide(a, b) {
     return(a / b);
 };
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function operate(op) {
+    switch (op) {
+    case "+":
+        add(firstNumber, secondNumber);
+        break;
+    case "-":
+        subtract(firstNumber, secondNumber);
+        break;
+    case "*":
+        multiply(firstNumber, secondNumber);
+        break;
+    case "/":
+        divide(firstNumber, secondNumber);
+        break;
+    };
+
+};
