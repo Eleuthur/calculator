d6977bfde7a8c49af4f92a294c8d2f5e160cf037
Eleuthur <105822177+Eleuthur@users.noreply.github.com>
2024-03-20 11:14:42 +0000

    Added functions to get button presses and update display

--- script.js
+++ script.js
@@ -35,3 +35,70 @@ function operate(op) {
     };
 
 };
+
+let display = "";
+
+const numberOne = document.querySelector('#b1');
+numberOne.addEventListener("click", () => {
+    display = display + "1";
+    updateDisplay();
+});
+
+const numberTwo = document.querySelector('#b2');
+numberOne.addEventListener("click", () => {
+    display = display + "2";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b3');
+numberOne.addEventListener("click", () => {
+    display = display + "3";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b4');
+numberOne.addEventListener("click", () => {
+    display = display + "4";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b5');
+numberOne.addEventListener("click", () => {
+    display = display + "5";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b6');
+numberOne.addEventListener("click", () => {
+    display = display + "6";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b7');
+numberOne.addEventListener("click", () => {
+    display = display + "7";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b8');
+numberOne.addEventListener("click", () => {
+    display = display + "8";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b9');
+numberOne.addEventListener("click", () => {
+    display = display + "9";
+    updateDisplay();
+});
+
+const numberOne = document.querySelector('#b0');
+numberOne.addEventListener("click", () => {
+    display = display + "0";
+    updateDisplay();
+});
+
+function updateDisplay() {
+    const displayText = document.querySelector('#display p');
+    displayText.textContent = display;
+};
